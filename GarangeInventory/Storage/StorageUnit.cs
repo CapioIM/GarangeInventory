f03f8b22c9f02b8843365ee82315957025723001
CapioIM <131615709+CapioIM@users.noreply.github.com>
2024-03-12 19:57:05 +0000

    storage and shelving Unit properties

---
+++ GarangeInventory/Storage/StorageUnit.cs
@@ -0,0 +1,23 @@
+﻿namespace GarangeInventory.StorageUnit
+{
+    public class StorageUnit
+    {
+		private int _idNumber;
+
+		public int IdNumber
+		{
+			get { return _idNumber; }
+			set { _idNumber = value; }
+		}
+
+		private string _name;
+
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+
+
+	}
+}
