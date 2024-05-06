d1eda5ed346371cb1d27e3bb3f48ca8edd479e0b
CapioIM <131615709+CapioIM@users.noreply.github.com>
2024-05-06 16:28:21 +0100

    transfered user services from data services in to UserData,cs

--- BlazorGI/Data/DataService.cs
+++ BlazorGI/Data/DataService.cs
@@ -5,34 +5,6 @@ namespace BlazorGI.Data
 {
     public class DataService
     {
-        private List<User> _users = new List<User>();
-
-        public List<User> Users
-        {
-            get { return _users; }
-        }
-
-
-        private bool GetUserAccess(string userName)
-        {
-            _users = TestData.TestDataUsers();
-            bool sucess = false;
-            foreach (User u in _users)
-            {
-                if (u.Name == userName)
-                {
-                    sucess = true;
-                }
-            }
-            return sucess;
-        }
-
-        public bool GetUserCheck(string userTypedName)
-        {
-            bool isUser = false;
-            isUser = GetUserAccess(userTypedName);
-            return isUser;
-        }
 
     }
 }
---
+++ BlazorGI/Data/UserData.cs
@@ -0,0 +1,50 @@
+﻿using GarangeInventory.Storage;
+using GarangeInventory;
+
+namespace BlazorGI.Data
+{
+    public class UserData
+    {
+        private List<User> _users = new List<User>();
+
+        public List<User> Users
+        {
+            get { return _users; }
+        }
+
+        private User _user;
+
+        public User User
+        {
+            get { return _user; }
+            set { _user = value; }
+        }
+
+
+        private bool GetUserAccess(string userName)
+        {
+            _users = TestData.TestDataUsers();
+            bool sucess = false;
+            foreach (User u in _users)
+            {
+                if (u.Name == userName)
+                {
+                    sucess = true;
+                }
+            }
+            return sucess;
+        }
+
+        public bool GetUserCheck(string userTypedName)
+        {
+            bool isUser = false;
+            isUser = GetUserAccess(userTypedName);
+            if (isUser)
+            {
+                
+            }
+            return isUser;
+        }
+
+    }
+}
