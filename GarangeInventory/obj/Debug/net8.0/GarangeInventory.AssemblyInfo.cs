375e72dd98c1cfb869d3ba2196bbd90f283512de
CapioIM <igorsmalinovskis9@gmail.com>
2024-05-21 15:26:05 +0100

    Merge with Conflicts

--- GarangeInventory/obj/Debug/net8.0/GarangeInventory.AssemblyInfo.cs
+++ GarangeInventory/obj/Debug/net8.0/GarangeInventory.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GarangeInventory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2ded2bab1ca390485efd7cd7da2dfd1d7f1dba5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+798d156a7486af0eea787710d8f502330cb6fe8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GarangeInventory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GarangeInventory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
