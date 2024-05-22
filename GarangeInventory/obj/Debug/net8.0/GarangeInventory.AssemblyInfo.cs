ebc325c13b9ac77b17be45387abef4c2fa04fb00
CapioIM <131615709+CapioIM@users.noreply.github.com>
2024-05-22 20:39:19 +0100

    refactor , passing parameters in to Components

--- GarangeInventory/obj/Debug/net8.0/GarangeInventory.AssemblyInfo.cs
+++ GarangeInventory/obj/Debug/net8.0/GarangeInventory.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GarangeInventory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4371d19a51bd090ab00f8e57e500ca33cf30264d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+364a2f09ffd8beffeccf76d6a51a6d0adb8332a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GarangeInventory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GarangeInventory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
