15a85b7c520d4c16879e7f819107847024ac0398
CapioIM <131615709+CapioIM@users.noreply.github.com>
2024-11-07 19:53:26 +0000

    Added button to access to test Page

--- GarangeInventory/obj/Debug/net8.0/GarangeInventory.AssemblyInfo.cs
+++ GarangeInventory/obj/Debug/net8.0/GarangeInventory.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GarangeInventory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+932e607b03fb82a6c82cc0bfc69ee5f72a741120")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47777a4a511f1bb84f69275d33964a3b1a98ab3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GarangeInventory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GarangeInventory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
