cfdd018829d0bd1fcb4ba63b25724f61a58c7d4f
CapioIM <131615709+CapioIM@users.noreply.github.com>
2025-03-10 22:30:20 +0000

    to find item location, 3levels out of 4

--- GarangeInventory/Expiry.cs
+++ GarangeInventory/Expiry.cs
@@ -36,40 +36,9 @@ namespace GarangeInventory
             return result;
         }
 
-        /// <summary>
-        /// shortens 3 lines from GetAllItemsLinq method
-        /// </summary>
-        /// <param name="storages"> List of storages </param>
-        /// <returns> returns List of ShelfUnits in storages </returns>
-        private static List<ShelfUnit> GetShelfUnits(List<StorageUnit> storages)
-        {
-            return storages
-                .SelectMany(storage => storage.ShelfUnits)
-                .ToList();
-        }
-
         public static List<Item> GetAllExpiredItems(List<Item> items)
         {
             return items.Where(item => item.Expiry.Date < DateTime.Now).ToList();
-        }
-
-        private static List<Item> GetItemsExpiryCheck(List<Item> items)
-        {
-            List<Item> result = new List<Item>();
-            if (items != null)
-            {
-                foreach (Item item in items)
-                {
-                    if (item != null)
-                    {
-                        if (item.Expiry.Date < DateTime.Now)
-                        {
-                            result.Add(item);
-                        }
-                    }
-                }
-            }
-            return result;
         }
 
         public static void DisplayExpiredItems(List<StorageUnit> storages)
