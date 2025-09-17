f3697cd1a8a6b577ff7548e33d449bc2822dd3b7
CapioIM <131615709+CapioIM@users.noreply.github.com>
2025-09-17 21:46:42 +0100

    implementing add new item feature

---
+++ BlazorGI/Components/TestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorGI.Components
+{
+    using System.ComponentModel.DataAnnotations;
+    public class TestModel
+    {
+        [Required]
+        public string? Name { get; set; }
+        [Range(1,100)]
+        public int? Quantity { get; set; }
+    }
+}
